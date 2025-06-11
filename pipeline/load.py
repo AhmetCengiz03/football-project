4b3e6d07c5b37420aa70ef5a6c782a1a1b4c0c07
JamezDon <jamesdono70@btopenworld.com>
2025-06-11 17:21:06 +0100

    Changes made based on review

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,21 +1,21 @@
 """Loading to database."""
-import os
+from os import environ as ENV
+
 from dotenv import load_dotenv
 import pandas as pd
-
 import psycopg2
-from psycopg2.extras import RealDictCursor, execute_values
+from psycopg2.extras import execute_values
 from psycopg2.extensions import connection
 
 
 def get_connection():
     """Get database connection to the PostgreSQL database."""
     conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
+        host=ENV("DB_HOST"),
+        dbname=ENV("DB_NAME"),
+        user=ENV("DB_USER"),
+        password=ENV("DB_PASSWORD"),
+        port=ENV("DB_PORT")
     )
     return conn
 
@@ -24,30 +24,20 @@ def insert_dataframe(df: pd.DataFrame, table_name: str, conn: connection) -> Non
     """Insert data from a pandas Dataframe into a PostgreSQL table"""
     cursor = conn.cursor()
     columns = ", ".join(df.columns)
-    # Need to convert the dataframe into a list of tuples for execute_values.
     values = df.to_records(index=False).tolist()
     insert_query = f"INSERT INTO {table_name} ({columns}) VALUES (%s)"
-
-    # Am trying to use execute_values here as it is quicker than inserting a record at a time.
     try:
-        execute_values(cursor, insert_query, values)
-        conn.commit()
+        with conn.cursor() as cursor:
+            execute_values(cursor, insert_query, values)
+            conn.commit()
     except psycopg2.Error as e:
         conn.rollback()
         print(f"Error inserting data: {e}")
-    finally:
-        cursor.close()
-
-
-def get_cursor(conn):
-    """Return a RealDictCursor."""
-    return conn.cursor(cursor_factory=RealDictCursor)
 
 
 def upload_all_data(transformed_data: dict[str, pd.DataFrame]):
-    """Upload json data to all relevant tables."""
+    """Upload transformed data to all relevant tables."""
     conn = get_connection()
-
     for table in [
         "team",
         "competition",
@@ -58,14 +48,11 @@ def upload_all_data(transformed_data: dict[str, pd.DataFrame]):
         "match_minute_stats",
         "match_event"
     ]:
-        df = transformed_data.get(table)
+        df = transformed_data["table"]
         insert_dataframe(df, table, conn)
-
     conn.close()
 
 
 if __name__ == "__main__":
     load_dotenv()
-    # Transformed_data_test is passed from transform script: dict[str, pd.DataFrame]
-    transformed_data_test = {}
-    upload_all_data(transformed_data_test)
+    upload_all_data(transformed_data)
