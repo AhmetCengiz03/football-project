4978045b9a93ae72b5b5de0e1deba54548c7e629
AhmetCengiz03 <cengizahmete@gmail.com>
2025-06-11 18:01:40 +0100

    Finalise output data

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -28,17 +28,38 @@ def get_all_daily_fixtures(conn: HTTPSConnection, config: dict) -> dict:
 def get_data_from_fixtures(conn: HTTPSConnection, config: dict) -> list[dict]:
     """Get the date from the fixtures"""
     data = get_all_daily_fixtures(conn, config)
-
+    matches_info = []
     for fixture in data:
         match_id = fixture["id"]
         league_id = fixture["league_id"]
         season_id = fixture["season_id"]
         fixture_name = fixture["name"]
         fixture_start_time = fixture["starting_at"]
-        home_participant_data = fixture["participants"][0]
-        away_participant_data = fixture["participants"][1]
+        location = fixture["meta"]["location"]
+        home_data = fixture["participants"][0]
+        away_data = fixture["participants"][1]
 
-    return [{"name": fixture["name"], "start": fixture["starting_at"], ""} for fixture in data]
+        home_team_id = home_data["id"]
+        home_name = home_data["name"]
+        home_code = home_data["short_code"]
+        home_image = home_data["image_path"]
+
+        away_team_id = away_data["id"]
+        away_name = away_data["name"]
+        away_code = away_data["short_code"]
+        away_image = away_data["image_path"]
+
+        matches_info.append({
+            "match_id": match_id,
+            "league_id": league_id,
+            "season_id": season_id,
+            "fixture_name": fixture_name,
+            "start_time": fixture_start_time,
+            "location": location,
+            "team_data": [{"home_team_id": home_team_id, "home_name": home_name, "home_code": home_code, "home_image": home_image},
+                          {"away_team_id": away_team_id, "away_name": away_name, "away_code": away_code, "away_image": away_image}]
+        })
+    return matches_info
 
 
 def connect_to_scheduler_client(config: dict) -> client:
@@ -47,18 +68,9 @@ def connect_to_scheduler_client(config: dict) -> client:
                   aws_secret_access_key=config["AWS_SECRET_ACCESS_KEY"])
 
 
-def create_event_bridge_schedules(fixtures: list[dict], scheduler_client: client, config: dict) -> list[dict]:
-    """Creates Eventbridge schedules for each fixture."""
-    results = []
-
-    for fixture in fixtures:
-        try:
-            start_time = fixture["start"]
-
-
 if __name__ == "__main__":
     load_dotenv()
     scheduler_client = connect_to_scheduler_client(ENV)
     api_conn = HTTPSConnection("api.sportmonks.com")
-    fixtures = get_start_times_from_fixtures(api_conn, ENV)
-    print(type(scheduler_client))
+    print(get_all_daily_fixtures(api_conn, ENV)[3])
+    fixtures = get_data_from_fixtures(api_conn, ENV)
