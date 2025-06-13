a88dde7b153f1018d8a7f26e5fb3005c9b62caf5
AhmetCengiz03 <cengizahmete@gmail.com>
2025-06-13 09:17:11 +0100

    Progress

--- architecture/terraform/infrastructure/main.tf
+++ architecture/terraform/infrastructure/main.tf
@@ -52,6 +52,20 @@ resource "aws_db_instance" "museum-db" {
     password = var.DATABASE_PASSWORD
 }
 
+# S3 BUCKET
+
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket = "c17-football-report-bucket"
+    force_destroy = true
+}
+
+resource "aws_s3_object" "report_folder" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  key    = "reports/"
+  content = ""
+}
+
+
 # ECR
 data "aws_ecr_image" "scheduler_image" {
     repository_name = "c17-football-scheduler-ecr"
@@ -81,4 +95,144 @@ data "aws_ecr_image" "report_image" {
 data "aws_ecr_image" "scheduler_stopper_image" {
     repository_name = "c17-football-scheduler-stopper-ecr"
     image_tag = "latest"
+}
+
+
+
+# LAMBDA
+
+resource "aws_iam_role" "lambda_role" {
+    name = "c17-football-lambda-role"
+    assume_role_policy = jsonencode({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "c17-football-lambda-policy"
+  description = "Allows Lambdas to run access S3, write logs, query a postgres RDS and allow SNS and SES messages."
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "S3Access",
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket",
+          "s3:DeleteObject",
+          "s3:GetBucketLocation"
+        ],
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.s3_bucket.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.s3_bucket.bucket}/*"
+        ]
+      },
+      {
+        Sid    = "CloudWatchLogs",
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      },
+      {
+        "Sid": "PublishSNSMessage",
+        "Effect": "Allow",
+        "Action": "sns:Publish",
+        "Resource": aws_sns_topic.report-topic.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_lambda_function" "report_lambda" {
+    depends_on = [ aws_iam_role_policy_attachment.lambda_role_attach ]
+    timeout = 120
+    memory_size = 512
+
+    function_name = "c17-cattus-report-lambda"
+    role          = aws_iam_role.lambda_role.arn
+
+    package_type = "Image"
+    image_uri = data.aws_ecr_image.report_image.image_uri
+
+    environment {
+        variables = {
+            DB_HOST=var.DB_HOST
+            DB_PORT=var.DB_PORT
+            DB_USER=var.DB_USER
+            DB_PASSWORD=var.DB_PASSWORD
+            DB_NAME=var.DB_NAME
+            DB_SCHEMA=var.DB_SCHEMA
+            TOPIC_REGION=var.AWS_REGION
+            TOPIC_ARN=aws_sns_topic.report-topic.arn
+        }
+    }
+}
+
+resource "aws_lambda_function" "short_pipeline_lambda" {
+    depends_on = [ aws_iam_role_policy_attachment.lambda_role_attach ]
+    timeout = 120
+    memory_size = 512
+
+    function_name = "c17-cattus-short-pipeline-lambda"
+    role          = aws_iam_role.lambda_role.arn
+
+    package_type = "Image"
+    image_uri = data.aws_ecr_image.short_pipe_image.image_uri
+
+    environment {
+        variables = {
+            DB_HOST=var.DB_HOST
+            DB_PORT=var.DB_PORT
+            DB_USER=var.DB_USER
+            DB_PASSWORD=var.DB_PASSWORD
+            DB_NAME=var.DB_NAME
+            DB_SCHEMA=var.DB_SCHEMA
+            S3_BUCKET=aws_s3_bucket.s3_bucket.bucket
+        }
+    }
+}
+
+resource "aws_lambda_function" "long_pipeline_lambda" {
+    depends_on = [ aws_iam_role_policy_attachment.lambda_role_attach ]
+    timeout = 120
+    memory_size = 512
+
+    function_name = "c17-cattus-long-pipeline-lambda"
+    role          = aws_iam_role.lambda_role.arn
+
+    package_type = "Image"
+    image_uri = data.aws_ecr_image.long_pipe_image.image_uri
+
+    environment {
+        variables = {
+            DB_HOST=var.DB_HOST
+            DB_PORT=var.DB_PORT
+            DB_USER=var.DB_USER
+            DB_PASSWORD=var.DB_PASSWORD
+            DB_NAME=var.DB_NAME
+            DB_SCHEMA=var.DB_SCHEMA
+            S3_BUCKET=aws_s3_bucket.s3_bucket.bucket
+        }
+    }
 }
