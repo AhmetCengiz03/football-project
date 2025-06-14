be500ab06e705ec9ecb59e4ada01c4899a9418a2
raf <rafhall@hotmail.com>
2025-06-14 13:27:10 +0100

    First clean.

--- pipeline/load.py
+++ pipeline/load.py
@@ -6,7 +6,6 @@ import pandas as pd
 import psycopg2
 from psycopg2.extras import execute_values
 from psycopg2.extensions import connection
-import numpy as np
 
 from transform import get_dataframe_from_json, transform_data
 
@@ -23,23 +22,26 @@ def get_connection() -> connection:
     )
 
 
-def insert_dataframe(df: pd.DataFrame, table_name: str, conn: connection, returning=None, conflict=None) -> list[tuple] | None:
+def insert_dataframe(df: pd.DataFrame, table_name: str, db_conn: connection,
+                     returning=None, conflict=None) -> list[tuple] | None:
     """Insert data from a pandas Dataframe into a PostgreSQL table"""
 
     columns = ", ".join(df.columns)
-    # values = df.to_records(index=False).tolist()
 
-    values = []
-    for _, row in df.iterrows():
-        processed_row = []
-        for item in row:
-            if pd.isna(item):
-                processed_row.append(None)
-            elif isinstance(item, float) and item.is_integer():
-                processed_row.append(int(item))
-            else:
-                processed_row.append(item)
-        values.append(tuple(processed_row))
+    if df.isnull().any().any():
+        values = []
+        for _, row in df.iterrows():
+            processed_row = []
+            for item in row:
+                if pd.isna(item):
+                    processed_row.append(None)
+                elif isinstance(item, float) and item.is_integer():
+                    processed_row.append(int(item))
+                else:
+                    processed_row.append(item)
+            values.append(tuple(processed_row))
+    else:
+        values = df.to_records(index=False).tolist()
 
     insert_query = f"INSERT INTO {table_name} ({columns}) VALUES %s"
 
@@ -51,143 +53,107 @@ def insert_dataframe(df: pd.DataFrame, table_name: str, conn: connection, return
     if returning:
         insert_query += f" RETURNING {returning}"
 
-    print(values)
-    print(columns)
-    print(insert_query)
     try:
-        with conn.cursor() as cursor:
+        with db_conn.cursor() as cursor:
 
             execute_values(cursor, insert_query, values)
 
             result = cursor.fetchall() if returning else None
 
-            conn.commit()
+            db_conn.commit()
             return result
 
     except psycopg2.Error as e:
-        conn.rollback()
+        db_conn.rollback()
         print(f"Error inserting data: {e}")
         return None
 
 
-def upload_all_data(transformed_data: dict[str, pd.DataFrame]) -> None:
+def upload_all_data(minute_df: pd.DataFrame, event_df: pd.DataFrame, db_conn: connection) -> None:
     """Upload transformed data to all relevant tables."""
-    conn = get_connection()
 
-    for table in [
-        "team",
-        "competition",
-        "season",
-        "match",
-        "match_assignment",
-        "event_type",
-        "match_minute_stats",
-        "match_event"
-    ]:
-        df = transformed_data["table"]
-        insert_dataframe(df, table, conn)
-    conn.close()
+    minute_stat_id = insert_dataframe(
+        minute_df, "match_minute_stats", db_conn, "minute_stat_id")
+
+    if isinstance(minute_stat_id, list):
+        event_df["minute_stat_id"] = minute_stat_id[0][0]
+
+    match_event_df = event_df[["match_event_id",
+                               "minute_stat_id", "event_type_id", "team_id"]]
+    insert_dataframe(match_event_df, "match_event",
+                     db_conn, "match_event_id", "match_event_id")
+
+    player_df = get_players_df(event_df)
+    insert_dataframe(player_df, "player", db_conn,
+                     returning="player_id", conflict="player_id")
+
+    player_match_event_df = event_df[["match_event_id",
+                                      "player_id", "related_player_id"]].copy()
+
+    player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].astype(
+        float).astype('Int64')
+
+    insert_dataframe(player_match_event_df, "player_match_event",
+                     db_conn, conflict=["match_event_id", "player_id"])
 
 
-def upload_match_event(df: pd.DataFrame, conn: connection):
+def get_players_df(event_df: pd.DataFrame):
     """Uploads the match event data."""
 
-    columns = ", ".join(df.columns)
-    values = df.to_records(index=False).tolist()
-    print(columns)
-    print(values)
+    players = pd.concat([
+        event_df[["player_id", "player_name"]].rename(
+            columns={"player_id": "id",
+                     "player_name": "name"}),
+        event_df[["related_player_id", "related_player_name"]].rename(
+            columns={"related_player_id": "id",
+                     "related_player_name": "name"})
+    ])
 
-    # query = """
-    #         INSERT INTO match_event
-    #         (match_event_id, minute_stat_id, event_type_id, team_id)
-    #         VALUES (%s, %s, %s, %s)
-    #         ON CONFLICT (match_event_id) DO NOTHING;
-    #         """
+    player_df = players.dropna(subset=["id"]).drop_duplicates(subset=["id"])
 
-    # with conn.cursor() as curs:
-    #     curs.execute()
+    return player_df.rename(
+        columns={"id": "player_id", "name": "player_name"})
 
 
 if __name__ == "__main__":
 
     load_dotenv()
 
-    base_df = get_dataframe_from_json("match_19367875/scrape_117.json")
-    a, b, c = transform_data(base_df)
-    print(a.info())
-    print(b)
+    base_df = get_dataframe_from_json("match_19348525/scrape_1.json")
+    minute_df, event_df, flags = transform_data(base_df)
 
-    db_conn = get_connection()
-    minute_stat_id = insert_dataframe(
-        a, "match_minute_stats", db_conn, "minute_stat_id")
+    conn = get_connection()
+    upload_all_data(minute_df, event_df, conn)
 
-    if isinstance(minute_stat_id, list):
-        b["minute_stat_id"] = minute_stat_id[0][0]
-    print(b)
+    # minute_stat_id = insert_dataframe(
+    #     minute_df, "match_minute_stats", db_conn, "minute_stat_id")
 
-    match_event_df = b[["match_event_id",
-                        "minute_stat_id", "event_type_id", "team_id"]]
+    # match_event_df = event_df[["match_event_id",
+    #                            "minute_stat_id", "event_type_id", "team_id"]]
 
-    print(insert_dataframe(match_event_df, "match_event",
-          db_conn, "match_event_id", "match_event_id"))
+    # players = pd.concat([
+    #     event_df[["player_id", "player_name"]].rename(
+    #         columns={"player_id": "id",
+    #                  "player_name": "name"}),
+    #     event_df[["related_player_id", "related_player_name"]].rename(
+    #         columns={"related_player_id": "id",
+    #                  "related_player_name": "name"})
+    # ])
 
-    players = pd.concat([
-        b[["player_id", "player_name"]].rename(
-            columns={"player_id": "id",
-                     "player_name": "name"}),
-        b[["related_player_id", "related_player_name"]].rename(
-            columns={"related_player_id": "id",
-                     "related_player_name": "name"})
-    ])
+    # player_df = players.dropna(subset=["id"]).drop_duplicates(subset=["id"])
+    # player_df = player_df.rename(
+    #     columns={"id": "player_id", "name": "player_name"})
 
-    player_df = players.dropna(subset=["id"]).drop_duplicates(subset=["id"])
-    player_df = player_df.rename(
-        columns={"id": "player_id", "name": "player_name"})
+    # print(insert_dataframe(player_df, "player", db_conn,
+    #       returning="player_id", conflict="player_id"))
 
-    print(insert_dataframe(player_df, "player", db_conn,
-          returning="player_id", conflict="player_id"))
-
-    player_match_event_df = b[["match_event_id",
-                               "player_id", "related_player_id"]].copy()
-    print(player_match_event_df)
-
-    player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].astype(
-        'Int64')
-    player_match_event_df["match_event_id"] = player_match_event_df["match_event_id"].astype(
-        int)
-    player_match_event_df["player_id"] = player_match_event_df["player_id"].astype(
-        int)
-
-    print(player_match_event_df)
-    print(player_match_event_df.dtypes)
+    # player_match_event_df = event_df[["match_event_id",
+    #                                   "player_id", "related_player_id"]].copy()
 
     # player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].astype(
-    #     'int', errors='ignore')
+    #     float).astype('Int64')
 
-    # player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].apply(
-    #     lambda x: int(x) if pd.notna(x) else None
-    # )
-    # player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].apply(
-    #     np.int64
-    # )
+    # insert_dataframe(player_match_event_df, "player_match_event",
+    #                  db_conn, conflict=["match_event_id", "player_id"])
 
-    player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].astype(
-        float).astype('Int64')
-    # player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].astype(
-    #     int)
-    # player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].astype(
-    #     str)
-    # player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].replace(
-    #     '-1', None)
-    # player_match_event_df["related_player_id"] = player_match_event_df["related_player_id"].astype(
-    #     int)
-
-    print(player_match_event_df)
-
-    # # print("Inserting player_match_event")
-    print(player_match_event_df.info())
-    insert_dataframe(player_match_event_df, "player_match_event",
-                     db_conn, conflict=["match_event_id", "player_id"])
-    db_conn.close()
-
-    # upload_all_data(transformed_data)
+    conn.close()
