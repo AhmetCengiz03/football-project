c4488725a6898146512c21a6db7aed0e45322d1e
raf <rafhall@hotmail.com>
2025-06-14 14:20:14 +0100

    Deal with game not started yet due to delay.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -10,22 +10,28 @@ from extract import run_extract
 from transform import get_dataframe_from_json, get_dataframe_from_response, transform_data
 from load import get_connection, upload_all_data
 
+
+def lambda_handler(event=None, context=None):
+    """Runs the ETL Pipeline."""
+    pass
+
+
 if __name__ == "__main__":
 
     load_dotenv()
 
     api_token = ENV["TOKEN"]
-    api_conn = HTTPSConnection("api.sportmonks.com")
+    api_conn = HTTPSConnection(ENV["BASE_URL"])
     db_conn = get_connection()
 
     raw_data = run_extract(19422412, api_token, api_conn)
 
-    df = get_dataframe_from_response(raw_data)
+    # df = get_dataframe_from_response(raw_data)
     df = get_dataframe_from_json("match_19348525/scrape_1.json")
-    try:
-        minute_df, event_df, flags = transform_data(df)
-    except IndexError as e:
-        print("Game has not started yet.")
-        exit()
 
-    upload_all_data(minute_df, db_conn, event_df)
+    if df["periods"].map(bool).any():
+        minute_df, event_df, flags = transform_data(df)
+        upload_all_data(minute_df, db_conn, event_df)
+
+    else:
+        print("Game has not started yet.")
